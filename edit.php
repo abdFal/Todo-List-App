952bd99b5e68b7cb8284735525242a724c717184
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-03-01 09:34:07 +0700

    Small Adjustment

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Todo List App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" href="/images/list-2389219_1280-removebg-preview.png" type="image/x-icon" >
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/e81773e2de.js" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Todo List App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" href="/images/Group 33.svg" type="image/x-icon" >
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/e81773e2de.js" crossorigin="anonymous"></script>
@@ -13,7 +13,7 @@
 
     <div class="container my-5">
       <div class="title my-4">
-        <h2 class="display-6 fw-semibold fs-3"><i class="fa-solid fa-list-ul text-warning"></i><span class="ms-3">Todo List</span></h2>
+        <h2 class="display-6 fw-semibold fs-3"><i class="fa-solid fa-list-ul text-primary-emphasis"></i><span class="ms-3">Todo List</span></h2>
       </div>
 <!-- card -->
     <div class="card shadow-lg p-1 mb-5 bg-body rounded border-dark">
@@ -56,7 +56,7 @@ $alldata = mysqli_query($connect, "SELECT * FROM list");
   </div>
   </footer>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="/js/main.js"></script>
   </body>
 </html>
