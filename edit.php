3bdc463e29c51a72a488cc8693a7652fb5c11e4e
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-03-02 10:00:22 +0700

    Update

--- edit.php
+++ edit.php
@@ -6,7 +6,7 @@
     <title>My Todo List App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" href="/images/list-2389219_1280-removebg-preview.png" type="image/x-icon" >
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <script src="https://kit.fontawesome.com/e81773e2de.js" crossorigin="anonymous"></script>
   </head>
 
--- index.php
+++ index.php
@@ -6,11 +6,10 @@
     <title>My Todo List App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" href="/images/Group 33.svg" type="image/x-icon" >
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <script src="https://kit.fontawesome.com/e81773e2de.js" crossorigin="anonymous"></script>
   </head>
   <body>
-
     <div class="container my-5">
       <div class="title my-4">
         <h2 class="display-6 fw-semibold fs-3"><i class="fa-solid fa-list-ul text-primary-emphasis"></i><span class="ms-3">Todo List</span></h2>
